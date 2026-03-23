e3f68f058d6ac209de313004020e078a777ba70b
freez3en <kondemist@gmail.com>
2026-03-23 11:18:41 +0300

    was add lock Contact and Footer

---
+++ main.js
@@ -0,0 +1,42 @@
+const projects = [
+  {
+    id: 1,
+    title: 'E-Commerce Platform',
+    description: 'Полнофункциональная платформа для интернет-магазина с корзиной, системой оплаты и админ-панелью.',
+    image: './img/e-commerce.jpg',
+    tags: ['Python', 'Flask', 'SQLAlchemy', 'JWT'],
+    link: 'https://github.com'
+  },
+  {
+    id: 2,
+    title: 'Task Management API',
+    description: 'RESTful API для управления задачами с полной документацией Swagger. Быстрый и масштабируемый backend на FastAPI.',
+    image: './img/task.jpg',
+    tags: ['Python', 'FastAPI', 'Docker', 'Postman'],
+    link: 'https://github.com'
+  },
+  {
+    id: 3,
+    title: 'Blog Platform',
+    description: 'Современная блог-платформа с системой комментариев, категорий и поиска. Построена на Django.',
+    image: './img/blog.jpg',
+    tags: ['Python', 'Django', 'SQLAlchemy', 'Docker'],
+    link: 'https://github.com'
+  },
+  {
+    id: 4,
+    title: 'Portfolio Dashboard',
+    description: 'Интерактивная панель управления для отслеживания портфолио проектов. Акцент на производительность.',
+    image: './img/dashboard.jpg',
+    tags: ['HTML', 'CSS', 'JavaScript', 'Flask'],
+    link: 'https://github.com'
+  },
+  {
+    id: 5,
+    title: 'Real-time Chat',
+    description: 'Чат-приложение с поддержкой real-time сообщений и групповых чатов. Безопасная авторизация через JWT.',
+    image: './img/project-chat.jpg',
+    tags: ['Python', 'FastAPI', 'JWT', 'Docker', 'WebSockets'],
+    link: 'https://github.com'
+  }
+];
